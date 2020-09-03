f12be108558b120901bd3e17d8f4bb2d2c65d1ef
Roberto Cavalcanti <63593378+rbcer17@users.noreply.github.com>
2020-09-02 22:51:26 -0300

    Rename exemplo1_Rogério.R to mod-1/exemplo1_Rogério.R

--- mod-1/exemplo1_Rogério.R
+++ mod-1/exemplo1_Rogério.R
@@ -12,9 +12,9 @@ summary(trees)
 #altura, volume e diametro das 30 arvores
 #Para calcular a media e variancia e necessario usar o comando "Mean", eu tentei puxar a media diretamente do objeto trees e nao funcionou
 #por isso e necessario utilizar o comando mean, colocar o objeto trees entre parentesis junto com o simbolo "$" para selecionar o vetor especifico
-#Depois é só repetir o processo para cada vetor pedindo a media
+#Depois Ã© sÃ³ repetir o processo para cada vetor pedindo a media
 #o mesmo processo deve ser feito usando o comando "var()"
-#Ao final de cada comando para ativar é preciso apertar as teclas "Ctrl e Enter".
+#Ao final de cada comando para ativar Ã© preciso apertar as teclas "Ctrl e Enter".
 #grave o script com um novo nome e extensao .R
 #sincronize com o seu repositorio no github e faca um pull request para 
 #o repositorio mestre
